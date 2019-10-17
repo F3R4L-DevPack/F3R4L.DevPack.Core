50320ea59d097b9ca75b545ed1760cca7000065b
Chris Franks <chris.franks@legendware.co.uk>
2019-10-17 20:32:50 +0100

    Work in progress

--- F3R4M.DevPack.Api/Services/ApiService.Get.cs
+++ F3R4M.DevPack.Api/Services/ApiService.Get.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using F3R4M.DevPack.Api.Endpoints;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace F3R4M.DevPack.Api.Services
 {
-    class ApiService
+    public partial class ApiService
     {
+        public async Task<T> GetAsync<T>(IApiEndpoint<T> apiEndpoint)
+        {
+            return new WebRequest
+        }
     }
 }
